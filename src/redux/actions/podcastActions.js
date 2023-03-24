0297fdf26d4d6614da7c2ef6677dff94c5163ced
Dani Alcala <daniamcode@gmail.com>
2023-03-24 23:03:41 +0100

    fetch timed data for individual podcasts

--- src/redux/actions/podcastActions.js
+++ src/redux/actions/podcastActions.js
@@ -47,3 +47,52 @@ export const loadPodcasts = () => {
       }  
   }
 }
+
+export const loadPodcastsDetails = ({podcastId}) => {
+  return async function (dispatch) {
+      let isLoading = true
+      // dispatch({
+      //   type: actionTypes.LOAD_PODCASTS_DETAILS,
+      //   payload: {
+      //       isLoading
+      //   }
+      // })
+      try {
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=10`)}`)
+          .then(response => {
+            if (response.ok) return response.json()
+            isLoading = false
+            console.error(response)
+              dispatch({
+                type: actionTypes.LOAD_PODCASTS_DETAILS,
+                payload: {
+                    error: response,
+                    isLoading
+                }
+              })
+          })
+          .then(data => {
+            isLoading = false
+            dispatch({
+              type: actionTypes.LOAD_PODCASTS_DETAILS,
+              payload: {
+                id:podcastId,
+                response: JSON.parse(data.contents).results,
+                isLoading,
+                timestamp: Date.now()
+              }
+            })
+          })
+    } catch (error) {
+          console.error(error)
+          isLoading = false
+            dispatch({
+                type: actionTypes.LOAD_PODCASTS_DETAILS,
+                payload: {
+                    error,
+                    isLoading
+                }
+            })
+        }  
+    }
+}
--- src/redux/reducers/podcastsReducer.js
+++ src/redux/reducers/podcastsReducer.js
@@ -9,6 +9,16 @@ const podcasts = (state = INITIAL, action = {}) => {
         ...state,
         podcastsList: action.payload
       }
+    case actionTypes.LOAD_PODCASTS_DETAILS:
+      // const newState = structuredClone(state) // fails 
+      const newState = {...state} // todo: use lodash' clonedeep
+      const newPodcastsDetailsIndex = newState?.podcastsDetails?.findIndex(el => el?.id === action?.payload?.id)
+      if(newPodcastsDetailsIndex && newPodcastsDetailsIndex !== -1) {
+        newState.podcastsDetails[newPodcastsDetailsIndex] = action?.payload
+      } else {
+        newState.podcastsDetails.push(action?.payload)
+      }
+      return newState
     default:
       return state
   }
